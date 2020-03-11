43e31637ea04bc21ed02a1fcb2bde30142144fd1
Louay Amr <louay_amr@outlook.com>
2020-03-11 13:26:12 +0200

    initial commit

---
+++ src/main/java/com/louay/projects/Main.java
@@ -0,0 +1,28 @@
+package com.louay.projects;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("please input the Sami's massage:  ");
+        StringBuilder s = new StringBuilder(input.next());
+
+        int counter = 0;
+        char temp;
+        char char1 = 'S';
+        char char2 = 'O';
+        System.out.print("\nThe Corruption letter is : ");
+        for (int i = 0; i < s.length(); i++) {
+            temp = s.charAt(i);
+            if (temp != char1 && temp != char2 ){
+                counter++;
+                System.out.print(temp);
+            }
+        }
+
+
+        System.out.println("\nThe number of letters that were altered : "+counter);
+
+    }
+}
